8fcae157f66e2a17280a690657f9d3fffaad984f
muhammed rifan <rifanmuhammed173@gmail.com>
2023-06-04 19:13:54 +0530

    deleted some files

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -8,16 +8,22 @@ Route.use(bodyparser.json());
 Route.use(bodyparser.urlencoded({ extended: true }))
 Route.set('view engine','ejs')
 Route.set('views', './views/admin-view');
+
+
+// rendering admin side pages
+
 Route.get('/',(req,res)=>{
-    res.render("dashboard")
+    res.render("messages")
 })
-Route.get('/billing',(req,res)=>{
-    res.render("billing")
+Route.get('/orders',(req,res)=>{
+    res.render("orders")
 })
 Route.get('/about',(req,res)=>{
     res.render('about')
-    //oooooolaaaaa
     
+})
+Route.get('/portfolio',(req,res)=>{
+    res.render('portfolio')
 })
 
 module.exports = Route
